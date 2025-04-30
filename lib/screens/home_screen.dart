8bf7ac9c2dc74b4ed539c37189194174d969ea2f
Godwin Umosen <umosengodwin568@gmail.com>
2025-04-30 15:16:45 +0100

    Third commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,33 +11,37 @@ class HomeScreen extends StatelessWidget {
       //we want it to be scrollable effect
       body: ListView(
         children: [
-          Column(children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    Text("Good Moring "),
-                    Text("Book Tickets ")
+          Container(
+            color: Colors.blue,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Text("Good Moring "),
+                      Text("Book Tickets ")
+                  ],
+                  ),
+            
+                  Container(
+                    color: Colors.red,
+                    width: 100,
+                    height: 70,
+                  )
                 ],
-                ),
-
-                Container(
-                  color: Colors.red,
-                  width: 100,
-                  height: 70,
-                )
-              ],
+              ),
+            
+               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Search icon"),
+                  Text("Empty space")
+                ],
+              )
+            ],
             ),
-
-             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Search icon"),
-                Text("Empty space")
-              ],
-            )
-          ],
           )
         ],
       ),
